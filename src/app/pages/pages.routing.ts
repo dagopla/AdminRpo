f2b4aadbea3b26b02d44df06cfde2f2280867269
dagopla <dagopla@gmail.com>
2022-08-02 15:14:15 -0500

    segundo commit

---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,33 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { ProgressComponent } from './progress/progress.component';
+
+
+
+const routes: Routes = [
+    {
+        path:'dashboard',
+        component:PagesComponent,
+        children:[
+          {path:'', component: DashboardComponent},
+          {path: 'progress', component:ProgressComponent },
+          {path: 'grafica1', component:Grafica1Component},
+          {path: '', redirectTo:'dashboard', pathMatch:'full'},
+        ]
+    },
+
+    //{ path: 'path/:routeParam', component: MyComponent },
+    //{ path: 'staticPath', component: ... },
+    //{ path: '**', component: ... },
+    //{ path: 'oldPath', redirectTo: '/staticPath' },
+    //{ path: ..., component: ..., data: { message: 'Custom' }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class PagesRoutingModule {}
